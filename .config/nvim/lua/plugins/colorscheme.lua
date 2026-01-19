f1b697e9665b4bca525266ce477b705908b28cc3
Paul Lavender-Jones <8403295+paullj@users.noreply.github.com>
2026-01-19 12:35:36 +0000

    feat: tmux sessionizer

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,4 +6,12 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
